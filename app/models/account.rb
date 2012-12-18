d8fa4c58f1d186c47b94101d89c742b3ec4cf876
jacky <jacky@jacky.(none)>
2012-12-18 17:54:50 +0800

    add bills

--- app/models/account.rb
+++ app/models/account.rb
@@ -12,6 +12,16 @@ class Account
   has_and_belongs_to_many :participants, :class_name => "User", inverse_of: :accounts_joined
   attr_accessible :restaurant, :total_fee, :status, :owner, :participants, :created, :modified, :cleared, :participant_ids
 
+  after_save :createBills
+
+  def createBills
+    participants.each do |p|
+      Bill.create :debtee=>owner, :debtor=>p, :account=>self, :type=>'Int', :fee => avg_fee, :status=>'New'
+      Bill.create :debtee=>owner, :debtor=>p, :account=>self, :type=>'Gap', :fee => gap_fee, :status=>'New'
+    end
+
+  end
+
   def avg_fee
     return 0 if participants.empty?
     total_fee.to_i / participants.size
