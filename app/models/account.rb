1f7daa8a1c65d62442550eb5b506e626f11ccfa0
jacky <jacky@jacky.(none)>
2012-12-19 10:37:17 +0800

    add delete dependent

--- app/models/account.rb
+++ app/models/account.rb
@@ -8,7 +8,7 @@ class Account
   field :status, type: String
 
   belongs_to :owner, :class_name => "User", inverse_of: :accounts_owned
-  has_many :bills
+  has_many :bills, dependent: :delete
   has_and_belongs_to_many :participants, :class_name => "User", inverse_of: :accounts_joined
   attr_accessible :restaurant, :total_fee, :status, :owner, :participants, :created, :modified, :cleared, :participant_ids
 
